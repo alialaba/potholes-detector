88d046c441e99088b101a24c682d4387f03b77b7
alialaba <alialaba079@gmail.com>
2023-06-22 15:10:19 -0400

    style navbar desktop hover state

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+const  navToggle= document.querySelector(".hamburger");
+const navbar = document.querySelector(".nav");
+
+const menuToggle =()=>{
+
+ // toggle aria-expanded if navbar has property of data visible
+ navbar.hasAttribute("data-visible") ?
+   navToggle.setAttribute("aria-expanded", false) :
+   navToggle.setAttribute("aria-expanded", true);
+   
+ navbar.toggleAttribute("data-visible");
+ navToggle.classList.toggle("active")
+ 
+}
+
+navToggle.addEventListener("click", menuToggle)
