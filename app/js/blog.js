459347510a4b202bf77bea8b160c8a7d1374c6fb
alialaba <alialaba079@gmail.com>
2024-03-13 07:08:48 -0400

    ft: make a api to get blogs data

--- app/js/blog.js
+++ app/js/blog.js
@@ -1,19 +1,63 @@
 const blogsEl = document.querySelector(".blog__posts");
-
-
 const url =  "http://localhost:3000/blogs";
-async  function getBlogs  (){
+
+
+//Make Api call
+async function getBlogs (){
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        showBlog(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getBlogs();
+
+//show blog logic
+const showBlog = (blogsData)=>{
+
+    //featured blog
+    const featuredBlog = blogsData[0]
+
+    //other blogs
+    const otherBlogs = blogsData.slice(1);
+
+
+     //didplay featured blog
+     displayBlog(featuredBlog, true);
+
+
+
+     //display other blogs
+     otherBlogs.forEach(blog => displayBlog(blog, false))
+    
 
 }
 
-const p = new Promise((resolve, reject)=>{
-    let isTrue = true;
-    if(isTrue){
-        resolve("Success");
-    }else{
-        reject("Error");
-    }
-})
 
-p.then(message =>(console.log("Promise Resolved")))
-.catch(message =>(console.log("Promise Rejected")))
+
+const displayBlog = (blog, isFeatured)=>{
+
+    //Create element for blog
+    const blogEl = document.createElement("div");
+    blogEl.classList.add("blog__column");
+
+
+    blogEl.innerHTML = `
+    
+    <img src="/images/blog-img1.svg" alt="blog__img"/>
+                <h3 class="blog__heading">${blog.title}</h3>
+                <p class="blog__post-content">${blog.content}</p>
+               <div class="blog__tags">
+                <span class="blog__tag blog__tag-author ">${blog.author}</span>
+                <span class="blog__tag blog__tag-read-time">${blog.read_time}</span>
+               </div>
+               <button class="btn btn-bg">Read more</button>
+    `
+
+    //Append child element
+    const parentEl = isFeatured ? document.querySelector(".blog__featured") : document.querySelector(".blog__recent");
+    parentEl.appendChild(blogEl)
+}
